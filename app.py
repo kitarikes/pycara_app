e6334f4ee921aa49d90fd5f676868abdc286f17e
kitarikes <ryosuke092413@gmail.com>
2019-12-14 21:16:04 +0900

    完成

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pyrebase
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -14,21 +14,38 @@ config = {
     "measurementId": "G-W581ZTLB6F"
 }
 
+#科目データ--------------------------------------------------
+data = {'math': 0 ,
+        'english': 0 , 
+        'history': 0 ,
+        'chemistry': 0 }
+#----------------------------------------------------------
+
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 auth = firebase.auth()
 storage = firebase.storage()
-# login関連--------------------------------------------------
+username = ''
+
+# user認証関連--------------------------------------------------
+
+#ユーザー新規登録
 @app.route("/sign_up_do", methods=["POST"])
 def sign_up_do():
         if request.method == "POST":
-                result = request.form
-                user = auth.create_user_with_email_and_password(result['email'], result['password'])
-                auth.send_email_verification(user['idToken'])
-                auth.refresh(user['refreshToken'])
-                data = {"name": result['email'].split('@')[0]}
-                db.child("users").child(result['email'].split('@')[0]).set(data)
-                return 'finished!!'
+                try:
+                        result = request.form
+                        user = auth.create_user_with_email_and_password(result  ['email'], result['password'])
+                        auth.send_email_verification(user['idToken'])
+                        auth.refresh(user['refreshToken'])
+                        username = result['email'].split('@')[0]
+                        db.child("users").child(result['email'].split('@')[0])  .set(data)
+                        db.child('users').child(username).set(data)
+                        message = '新規作成完了しました！このままログインして下さい。'
+                        return render_template('login/sign_in.html', message=message)
+                except:
+                        error = '既に使われているメールアドレスか、入力に誤りがあります。'
+                        return render_template('login/sign_up.html', error=error)
         else:
                 render_template('login/sign_up.html')
 
@@ -36,37 +53,45 @@ def sign_up_do():
 def sign_up():
     return render_template('login/sign_up.html')
 
+
+#ログイン
 @app.route("/sign_in")
 def sign_in():
         return render_template('login/sign_in.html')
 
 @app.route("/sign_in_do", methods=['POST'])
 def sign_in_do():
-        result = request.form
-        auth.sign_in_with_email_and_password(result['email'], result['password'])
-        return ''
+        try:
+
+                result = request.form
+                auth.sign_in_with_email_and_password(result['email'], result    ['password'])
+                #sign_in したusernameを取得
+                global username
+                username = result['email'].split('@')[0]
+                return redirect('/subject')
+        except:
+                return redirect('/sign_in')
+
+
+#ログアウト
+@app.route("/sign_out")
+def sign_out():
+        username = ''
+        return redirect('/sign_in')
+        
 #------------------------------------------------------------
 
-@app.route('/mypage', methods=["GET", "POST"])
-def mypage():
-        if request.method == "GET":
-                print(1)
-        return ''
+#教科リストの表示とお気に入り機能--------------------------------
+@app.route("/subject", methods=['GET','POST'])
+def sub_home():
+        if username == '':
+                return redirect('/sign_in')
 
-
-#お気に入り機能----------------------------------------------
-@app.route("/pre", methods=['GET','POST'])
-def home():
-        username = 'hogehogehoge'
-        if request.method == "GET":
-                dict = db.child('subject_url').get().val()
-                k = []
-                for key, value in dict.items():
-                        k.append(key)
-
+        elif request.method == "GET":
+                k = data.keys()
                 sub_list = db.child('users').child(username).get().val()
                 
-                return render_template('subject/show.html', k=k, sub_list=sub_list)
+                return render_template('subject/list.html', k=k, sub_list=sub_list, username = username)
         else:
                 result = request.form
 
@@ -74,65 +99,55 @@ def home():
                 a = result.to_dict().keys()
                 for i in a:
                         sub_name = i
-                db.child('users').child(username).set({sub_name : int(result[sub_name])})
+                data[sub_name] = int(result[sub_name])
+                db.child('users').child(username).set(data)
 #--------------------------------------------------------------
 
-                dict = db.child('subject_url').get().val()
-                k = []
-                for key, value in dict.items():
-                        k.append(key)
-
+                k = data.keys()
                 sub_list = db.child('users').child(username).get().val()
                 
-                return render_template('subject/show.html', k=k, sub_list=sub_list)
+                return render_template('subject/list.html', k=k, sub_list=sub_list, username = username)
                 
-#お気に入り機能----------------------------------------------
+#お気に入り機能-------------------------------------------------
 
 
-#画像アップロード関連----------------------------------------
-@app.route("/pre_upload")
-def img_up():
-        return render_template('img/pre_upload.html')
-
-@app.route("/pre_upload_do", methods=['POST'])
-def up():
-        subject = "math"
-        f = request.files["s_file"]
-        print(f)
-        storage.child("images").child(subject).child(f.filename).put(f)
-        url = storage.child("images/math/"+f.filename).get_url(token=None)
-        data = {"url":url}
-        db.child("subject_url").child("math").push(data)
-
-        return ''
-
-@app.route('/show')
-def show():
-        my_dic = db.child("subject_url").child("math").get().val()
+#教科ごとの画像表示
+@app.route('/subject/<sub_name>')
+def subject_detail(sub_name):
         a = []
         ans = []
-        for key, value in my_dic.items():
-                a.append(key)
-                ans.append(value)
         k =[]
-        for i in ans:
-                k.append(i['url'])
+        try:
+                my_dic = db.child("subject_url").child(sub_name).get().val()
+                for key, value in my_dic.items():
+                        a.append(key)
+                        ans.append(value)
+                for i in ans:
+                        k.append(i['url'])
+        except:
+                pass
 
 
+        return render_template('subject/show.html', k = k, sub_name=sub_name)       
 
-        return render_template('show.html', k = k)
-#------------------------------------------------------------
+#教科ごとの画像upload form
+@app.route('/subject/<sub_name>/upload', methods=['GET', 'POST'])
+def upload(sub_name):
+        if request.method == "GET":
+                return render_template('subject/upload.html',   sub_name=sub_name)
+
+        else:
+                subject = sub_name
+                f = request.files["s_file"]
+                storage.child("images").child(subject).child(f.filename).put(f)
+                url = storage.child("images/"+subject+"/"+f.filename).get_url(token=None)
+                data = {"url":url}
+                db.child("subject_url").child(subject).push(data)
+                return redirect("/subject/"+subject)
 
 @app.route('/')
-def pre1():
-        username='hogehogehoge'
-        sub_list = db.child('users').child(username).get().val()
-        print(sub_list['English'])
-        return ''
-
-
-
-
+def home():
+        return redirect('/sign_in')
 
 app.run(host="localhost", debug=True)
 
